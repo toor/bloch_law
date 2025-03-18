62ecf7f09aa1b940acf996bd724ad8ea90b40871
William <william@huddie.net>
2025-03-18 16:53:16 +0100

    Add all lattice basis vectors and symmetry directions; working on a method to systematically generate nn vectors in a given plane (WIP)

--- generate_lattices.py
+++ generate_lattices.py
@@ -2,46 +2,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import sys
+import os
+
+def gen_rhombohedral(a, alpha):
+    return a*np.array([[1, 0, 0],
+                       [np.cos(alpha), np.sin(alpha), 0],
+                       [np.cos(alpha), (1-np.cos(alpha))*np.cos(alpha)/np.sin(alpha), np.sqrt((1-np.cos(alpha))(1 + 2*np.cos(alpha))/(1 + np.cos(alpha)))]], dtype=np.float64) 
+def gen_triclinic(a, b, c, alpha, beta, gamma): 
+    return np.array([[a, 0, 0],
+                    [b*np.cos(gamma), b*np.sin(gamma), 0],
+                    [c*np.cos(beta),
+                     c*(np.cos(alpha) - np.cos(beta)*np.cos(gamma)) / np.sin(gamma),
+                     c * np.sqrt(1 - np.cos(beta)**2 - ((np.cos(alpha) - np.cos(beta)*np.cos(gamma)) / np.sin(gamma))**2)]], dtype=np.float64)
 
 def get_basis_vectors(lattice, a, b, c, alpha, beta, gamma):
-    lambda gen_rhombohedral: a,alpha: a*np.array([[1, 0, 0],
-                                                  [np.cos(alpha), np.sin(alpha), 0],
-                                                  [np.cos(alpha), (1-np.cos(alpha)*np.cos(alpha)/np.sin(alpha), np.sqrt((1-np.cos(alpha))(1 + 2*np.cos(alpha))/(1 + np.cos(alpha)))]]) 
-    lambda gen_triclinic: a, b, c, alpha, beta, gamma: np.array([a, 0, 0],
-                                                                [b*np.cos(gamma), b*np.sin(gamma), 0],
-                                                                [c*np.cos(beta),
-                                                                 c*(np.cos(alpha) - np.cos(beta)*np.cos(gamma)) / np.sin(gamma),
-                                                                 c * np.sqrt(1 - np.cos(beta)**2 - ((np.cos(alpha) - np.cos(beta)*np.cos(gamma)) / np.sin(gamma))**2)])
 
     match lattice:
         case "cP":
-            return np.array([[a, 0, 0], [0, a, 0], [0, 0, a]])
+            return (np.array([[a, 0, 0], [0, a, 0], [0, 0, a]], dtype=np.float64), np.array([[1,0,0], [0,1,0], [0,0,1], [1,1,0], [1,1,1]], dtype=np.float64))
         case "cI":
-            return (a/2)*np.array([[-1, 1, 1], [1, -1, 1], [1, 1, -1]])
+            return ((a/2)*np.array([[-1, 1, 1], [1, -1, 1], [1, 1, -1]], dtype=np.float64), np.array([[1,0,0], [0,1,0], [0,0,1], [1,1,0], [1,1,1]], dtype=np.float64))
         case "cF":
-            return (a/2)*np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
+            return ((a/2)*np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]], dtype=np.float64), np.array([[1,0,0], [0,1,0], [0,0,1], [1,1,0], [1,1,1]], dtype=np.float64))
         case "tP":
-            return np.array([[a, 0, 0], [0, a, 0], [0, 0, c]])
+            return (np.array([[a, 0, 0], [0, a, 0], [0, 0, c]], dtype=np.float64), np.array([[1,0,0], [0,1,0], [0,0,1], [1,1,0]], dtype=np.float64))
         case "tI":
-            return np.array([[a, -a, c], [a, a, c], [-a, -a, c]]) / 2
+            return (np.array([[a, -a, c], [a, a, c], [-a, -a, c]], dtype=np.float64) / 2, np.array([[1,0,0], [0,1,0], [0,0,1], [1,1,0], [1,1,1]], dtype=np.float64))
         case "oP":
-            return np.array([[a, 0, 0], [0, b, 0], [0, 0, c]])
+            return (np.array([[a, 0, 0], [0, b, 0], [0, 0, c]], dtype=np.float64), np.array([[1,0,0], [0,1,0], [0,0,1], [1,1,0], [0,1,1]], dtype=np.float64))
         case "oS":
-            return np.array([[a/2, b/2, 0], [-a/2, b/2, 0], [0, 0, c]])
+            return (np.array([[a/2, b/2, 0], [-a/2, b/2, 0], [0, 0, c]], dtype=np.float64), np.array([[1,0,0], [0,1,0], [0,0,1], [1,1,0], [0,1,1]], dtype=np.float64)) 
         case "oI":
-            return np.array([[a, b, c], [-a, b, c], [a, -b, c]]) / 2
+            return (np.array([[a, b, c], [-a, b, c], [a, -b, c]], dtype=np.float64) / 2, np.array([[1,0,0], [0,1,0], [0,0,1], [1,1,0], [1,1,1]], dtype=np.float64))
         case "oF":
-            return np.array([[0, b, c], [a, 0, c], [a, b, 0]]) / 2
+            return (np.array([[0, b, c], [a, 0, c], [a, b, 0]], dtype=np.float64) / 2, np.array([[1,0,0], [0,1,0], [0,0,1], [1,1,0], [0,1,1]], dtype=np.float64))
         case "hP":
-            return np.array([[a, 0, 0], [-a/2, a*np.sqrt(3)/2, 0], [0, 0, c]])
+            return (np.array([[a, 0, 0], [-a/2, a*np.sqrt(3)/2, 0], [0, 0, c]], dtype=np.float64), np.array([[1,0,0], [0,1,0], [0,0,1], [1,1,0]], dtype=np.float64))
         case "hR":
-            return gen_rhombohedral(a, alpha)
+            return (gen_rhombohedral(a, alpha), np.array([[1,1,1], [1,1,0]], dtype=np.float64))
         case "mP":
-            return np.array([[a, 0, 0], [0, b, 0], [c*np.cos(beta), 0, c*np.sin(beta)]])
+            return (np.array([[a, 0, 0], [0, b, 0], [c*np.cos(beta), 0, c*np.sin(beta)]], dtype=np.float64), np.array([[1,0,0], [0,1,0], [0,0,1]], dtype=np.float64))
         case "mC":
-            return np.array([[a/2, b/2, 0], [-a/2, b/2, 0], [c*np.cos(beta), 0, c*np.sin(beta)]])
+            return (np.array([[a/2, b/2, 0], [-a/2, b/2, 0], [c*np.cos(beta), 0, c*np.sin(beta)]], dtype=np.float64), np.array([[1,0,0], [0,1,0], [0,0,1]], dtype=np.float64))
         case "aP":
-            return gen_triclinic(a, b, c, alpha, beta, gamma)
+            return (gen_triclinic(a, b, c, alpha, beta, gamma), None)
 
     #def get_bravais_lattices(a, b, c, alpha, beta, gamma, theta): 
     #    lattices = {
@@ -126,53 +130,6 @@ def get_basis_vectors(lattice, a, b, c, alpha, beta, gamma):
     #    }
     #    return lattices
 
-def generate_high_symmetry_directions():
-    cells = {
-        # Cubic
-        "sc_100": np.array([1,0,0]),
-        "bcc_100": np.array([1,0,0]),
-        "bcc_110": np.array([1,1,0]),
-        "bcc_111": np.array([1,1,1]),
-        "fcc_100": np.array([1,0,0]),
-        "fcc_110": np.array([1,1,0]),
-        "fcc_111": np.array([1,1,1]),
-        # Simple tetragonal
-        "st_100": np.array([1,0,0]),
-        "st_110": np.array([1,1,0]), 
-        "st_001": np.array([0,0,1]),
-        # Body-centred tetragonal
-        "bct_100": np.array([1,0,0]),
-        "bct_010": np.array([0,1,0]),
-        "bct_001": np.array([0,0,1]),
-        # Simple orthorhombic 
-        "so_100": np.array([1,0,0]),
-        "so_010": np.array([0,1,0]),
-        "so_001": np.array([0,0,1]),
-        # Base-centred orthorhombic is the same as simple orthorhombic
-        "baco_100": np.array([1,0,0]),
-        "baco_010": np.array([0,1,0]),
-        "baco_001": np.array([0,0,1]),
-        # Body-centred orthorhombic 
-        "bco_100": np.array([1,0,0]),
-        "bco_010": np.array([0,1,0]),
-        "bco_001": np.array([0,0,1]),
-        "bco_110": np.array([1,1,0]),
-        "bco_111": np.array([1,1,1]),
-        # Hexagonal
-        "sh_100": np.array([1,0,0]),
-        "sh_010": np.array([0,1,0]),
-        "sh_110": np.array([1,1,0]),
-        "sh_001": np.array([0,0,1]),
-        # Rhombohedral
-        "rh_111": np.array([1,1,1]),
-        "rh_110": np.array([1,1,0]),
-        # Monoclinic 
-        "mc_100": np.array([1,0,0]),
-        "mc_001": np.array([0,0,1])
-    }
-   
-    return cells
-
 def plot_plane(fig, ax, plane_z):
     x = np.linspace(-1, 1, 100)
     y = np.linspace(-1, 1, 100)
@@ -202,6 +159,7 @@ def plot_lattice(points, filename, vectors=None):
     ax.set_zticks(ticks=[0.0, 1.0, -1.0], labels=['z=0', '1', '-1'])
 
     fig.savefig(filename)
+    plt.close()
 
 def rot_matrix(miller_indices, rot_axis):
     print(f'Rotating {miller_indices} to align with axis {rot_axis}')
@@ -233,8 +191,8 @@ def rot_matrix(miller_indices, rot_axis):
 # Rotate the lattice so that a particular crystallographic direction is aligned with the 'z'
 # axis in the original coordinate system.
 def rotate_lattice(points, direction, align_axis):
-    miller_indices = np.array(direction, dtype=np.float64)
-    rot_axis = np.array(align_axis, dtype=np.float64)
+    miller_indices = direction
+    rot_axis = align_axis
     R = rot_matrix(miller_indices, rot_axis)
 
     for i in range(points.shape[0]):
@@ -245,7 +203,10 @@ def rotate_lattice(points, direction, align_axis):
     return points
 
 def generate_lattice(lattice_vectors, rep_x, rep_y, rep_z):
-    a1, a2, a3 = lattice_vectors
+    print(lattice_vectors)
+    a1 = lattice_vectors[0,:]
+    a2 = lattice_vectors[1,:]
+    a3 = lattice_vectors[2,:]
     points = []
     for i in range(-rep_x, rep_x+1):
         for j in range(-rep_y, rep_y + 1):
@@ -286,6 +247,77 @@ def in_plane_vectors_original_basis(lattice, plane, miller_indices, rot_axis):
     shortest_vectors = rotate_lattice(np.array(shortest_vectors), rot_axis, miller_indices)
 
     return return_2d_vectors(shortest_vectors)
+
+def calculate_in_plane_vectors(lat, a, b, c, alpha, beta, gamma, prefix):
+    rep_x = rep_y = rep_z = 3
+    z_axis = np.array([0,0,1], dtype=np.float64)
+
+    (basis_vectors, symmetry_directions) = get_basis_vectors(lat, a, b, c, alpha, beta, gamma)
+    lattice = generate_lattice(basis_vectors, rep_x, rep_y, rep_z)
+    image_file = prefix + "unrotated_lattice.pdf"
+    plot_lattice(lattice, image_file, vectors=None)
+
+    for i, _ in enumerate(symmetry_directions):
+        print(f"Generating in-plane nearest neighbour vectors for lattice {lat} with plane defined by high-symmetry direction {symmetry_directions[i].astype(np.int64)}\n")
+        axis = symmetry_directions[i]
+        axis_string = str(int(axis[0] * 100 + axis[1]*10 + axis[0]*1))
+        
+        #print(f"Axis = {axis}, aligning to {z_axis}")
+        # Rotate the lattice so that the given symmetry direction is aligned along 
+        # the vertical axis of the figure
+        if np.array_equal(axis, z_axis):
+            #print("Axes are already aligned; skipping this symmetry.\n")
+            z0_plane = select_atoms_by_plane(lattice, z=0)
+        else:
+            rotated_lattice = rotate_lattice(lattice, axis, z_axis)
+            # Select all lattice points (atoms) that lie in the plane z = 0
+            z0_plane = select_atoms_by_plane(rotated_lattice, z=0)
+        
+        # Sort these vectors by length and isolate the N shortest ones.
+        sorted_vectors = sort_by_distance(z0_plane, 0.0, 0.0)
+        print(sorted_vectors)
+        min_length = np.linalg.norm(sorted_vectors[1,:])
+        shortest_vectors = []
+        for vec in sorted_vectors:
+            if np.isclose(np.linalg.norm(vec), min_length):
+                shortest_vectors.append(vec)
+        shortest_vectors = np.array(shortest_vectors)
+        # Again check if the symmetry axis is already along z.
+        if np.array_equal(axis, z_axis):
+            #print("Axes are already aligned; skipping this symmetry.\n")
+            shortest_vectors_rot = shortest_vectors
+        else:
+            # Return to the original coordinate system
+            shortest_vectors_rot = rotate_lattice(shortest_vectors, z_axis, axis)
+
+        # Plot lattice projection and save image
+        image_file = prefix + f"{axis_string}_lattice.pdf"
+        dat_file = prefix + f"{axis_string}_nn_vectors.dat"
+        plot_lattice(z0_plane, image_file, vectors=shortest_vectors)
+        # Write nearest neighbour vectors to file.
+        with open(dat_file, "w") as f:
+            f.write("Index    x    y    z\n")
+            for i, vec in enumerate(shortest_vectors_rot):
+                f.write(f"{i}    {vec[0]}    {vec[1]}    {vec[2]}\n")
+            f.close()
+
+a = 1.0 
+b = 1.5
+c = 2.0
+alpha = np.pi/4
+beta = np.pi/3
+gamma = np.pi/6
+
+lattice_types = ["cP", "cI", "cF", "tP", "tI", "oP", "oS", "oI", "oF", "hP", "hR", "mP", "mS"]
+
+for lat in lattice_types:
+    prefix = f"nn_data_{lat}/"
+    os.makedirs(os.getcwd() + "/" + prefix, exist_ok=True)
+
+    calculate_in_plane_vectors(lat, a, b, c, alpha, beta, gamma, prefix)
+
+sys.exit(0)
+
 
 
 def main(lattice_type, alignment, a, b, c, alpha, beta, gamma, theta, filename):
