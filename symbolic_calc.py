52c08394c4e963008109e4b08840e0f274ab635f
William <william@huddie.net>
2025-06-02 15:50:38 +0200

    Represent symmetry directions as strings for easy reading from file

--- symbolic_calc.py
+++ symbolic_calc.py
@@ -7,40 +7,23 @@ sqrt3 = sqrt(3)
 
 def arr_f64(arr):
     return np.array(arr, dtype=np.float64)
+kpar = Matrix([k_x, k_y])
+
+
+# We basically want to evaluate the sum over nearest neighbours in:
+# Case 1) The bulk. Must sum over in-plane nearest neighbours and out-of-plane.
+# Case 2) The bottom surface (n=1). Must sum over in-plane neighbours and those in the plane above (n=2).
+# Case 3) The top surface (n=N). Must sum over in-plane neighbours and those in the plane below (n = N - 1).
 
 class Basis:
     def __init__(self, lattice_type):
         match lattice_type:
             case "cP":
                 self.basis = Matrix([[a,0,0],[0,a,0],[0,0,a]])
-                self.sym_dir = arr_f64([[1,0,0],[0,1,0],[1,1,0],[1,1,1]])
+                self.sym_dir = ["100", "010", "110", "111"]
             case "cI":
                 self.basis = a*half*Matrix([[-1,1,1],[1,-1,1],[1,1,-1]])
-                self.sym_dir = arr_f64([[1,0,0],[0,1,0],[1,1,0],[1,1,1]])
+                self.sym_dir = ["100", "010", "110", "111"]
             case "cF":
                 self.basis = a*half*Matrix([[0,1,1],[1,0,1],[1,1,0]])
-                self.sym_dir = arr_f64([[1,0,0],[0,1,0],[1,1,0],[1,1,1]])
-            case "tP":
-                self.basis = Matrix([[a,0,0],[0,a,0],[0,0,c]])
-                self.sym_dir = arr_f64([[1,0,0],[0,1,0],[1,1,0]])
-            case "tI":
-                self.basis = Matrix([[a,-a,c],[a,a,c],[-a,-a,c]])
-                self.sym_dir = arr_f64([[1,0,0],[0,1,0],[1,1,0],[1,1,1]])
-            case "oP":
-                self.basis = Matrix([[a,0,0],[0,b,0],[0,0,c]])
-                self.sym_dirs = arr_f64([[1,0,0,],[0,1,0],[1,1,0],[1,1,1]])
-            case "oS":
-                self.basis = Matrix([[a*half, b*half, 0], [-a*half, b*half, 0], [0,0,c]])
-                self.sym_dirs = arr_f64([[1,0,0],[0,1,0],[1,1,0],[1,1,1]])
-            case "oI":
-                self.basis = Matrix([[a,b,c],[-a,b,c],[a,-b,c]])
-                self.sym_dirs = arr_f64([[1,0,0],[0,1,0],[1,1,0],[1,1,1]])
-            case "oF":
-                self.basis = Matrix([[0,b,c],[a,0,c],[a,b,0]])
-                self.sym_dirs = arr_f64([[1,0,0],[0,1,0],[1,1,0],[1,1,1]])
-            case "hP":
-                self.basis = Matrix([[a,0,0],[-half*a, a*sqrt3*half, 0], [0,0,c]])
-                self.sym_dirs = arr_f64([[1,0,0],[0,1,0],[1,1,0]])
-            case "hR":
-                self.basis =  
-
+                self.sym_dir = ["100", "010", "110", "111"]
