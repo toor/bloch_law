Fixed bug due to floating point precision that gave incorrect answers for Bravais coefficients

lattice.py
@@ -229,7 +229,7 @@ class Lattice:
         ax.set_xticks(ticks=[0.0, 1.0, -1.0], labels=['x=0', '1', '-1'])
         ax.set_yticks(ticks=[0.0, 1.0, -1.0], labels=['y=0', '1', '-1'])
         ax.set_zticks(ticks=[0.0, 1.0, -1.0], labels=['z=0', '1', '-1'])
-        plt.show()
+        #plt.show()
 
         fig.savefig(filename)
         plt.close()
@@ -324,7 +324,8 @@ class Lattice:
             print_r_text(f"(x,y,z) = ({vector[0]:.4f}, {vector[1]:.4f}, {vector[2]:.4f})\n")
         else:
             print(f"(x,y,z) = ({vector[0]:.4f}, {vector[1]:.4f}, {vector[2]:.4f})\n")
-    
+    def as_integer_tuple(self, vector):
+        return np.array([int(round(vector[0])), int(round(vector[1])), int(round(vector[2]))])
 
     # For every nn_vector v_i, we have v_i = M (n1, n2, n3)^T, 
     # where n1, n2 and n3 are integer coefficients of the basis vectors, 
@@ -333,27 +334,28 @@ class Lattice:
     def nearest_neighbours_original_basis(self, sym_dir, vector):
         #print(f"Computing integer coefficients of nearest neighbours for lattice {self.ltype} with symmetry direction {sym_dir}")
         basis = self.original_basis 
-        a1 = basis[0,:]#.reshape((3,1))
-        a2 = basis[1,:]#.reshape((3,1))
-        a3 = basis[2,:]#.reshape((3,1))
+        a1 = basis[0,:].reshape((3,))
+        a2 = basis[1,:].reshape((3,))
+        a3 = basis[2,:].reshape((3,))
 
         A = np.column_stack((a1,a2,a3))
         #print(A)
         R = self.rot_matrix(self.string_to_sym_dir(sym_dir), inverse=True)
-        print(f'R shape = {R.shape}')
+        #print(f'R shape = {R.shape}')
 
         A_inv = np.linalg.inv(A)
-        print("Matrix to compute basis coefficients is: \n")
-        print(A_inv)
+        #print("Matrix to compute basis coefficients is: \n")
+        #print(A_inv)
         #print("Vector to be rotated:\n")
-        self.print_vector(vector)
+        #self.print_vector(vector)
         #print("Rotation of vector:\n")
-        self.print_vector(R @ vector, True)
         rot_vector = R @ vector
-        print(f'rotated vector shape = {rot_vector.shape}')
+        #self.print_vector(rot_vector)
+        #print(f'rotated vector shape = {rot_vector.shape}')
         # first return the vector to the original basis, then compute it 
         # in terms of integer coefficients of the original basis vectors.
         v =  A_inv @ rot_vector 
-        return v.astype(np.int64)
+        #self.print_vector(v)
+        return self.as_integer_tuple(v)
 
 

main.py
@@ -27,11 +27,11 @@ for lattice_type in lattice_types:
     nearest_neighbours = lattice.in_plane_nearest_neighbours(3, plot_image=True, prefix=prefix)
     
     for s, nns in nearest_neighbours.items():
-        print(f"Symmetry direction: {s}")
+        #print(f"Symmetry direction: {s}")
         data_dir = prefix + f"/nn_{lattice_type}/{s}/"
         #print(data_dir)
         os.makedirs(data_dir, exist_ok=True)
-        filename = data_dir + f"{s}_nn_inplane.dat"
+        filename = data_dir + "nn_ip.dat"
         
         (in_plane, out_of_plane) = nns
         print(f"{in_plane.shape[0]}, {out_of_plane.shape[0]}")
@@ -43,6 +43,7 @@ for lattice_type in lattice_types:
             f.write('idx    n1    n2    n3\n')
            
             for i, nn in enumerate(in_plane):
+                print(f"nn shape: {nn.shape}")
                 coeffs = lattice.nearest_neighbours_original_basis(s, nn)
                 
                 #print("Has Bravais lattice coefficients (in conventional basis):\n")
@@ -52,7 +53,7 @@ for lattice_type in lattice_types:
 
             f.close()
 
-        filename = data_dir + f"{s}_nn_outofplane.dat"
+        filename = data_dir + "nn_oop.dat"
         
         print("Computing out-of-plane nearest neighbours in original basis\n")
         with open(filename, "w") as f:
@@ -60,17 +61,18 @@ for lattice_type in lattice_types:
 
             for i, nn in enumerate(out_of_plane):
                 coeffs = lattice.nearest_neighbours_original_basis(s, nn)
-                #print("Has Bravais lattice coefficients (in conventional basis):\n")
+                print("Has Bravais lattice coefficients (in conventional basis):\n")
                 lattice.print_vector(coeffs, True)
+                #print(f"int of -1 is {int(-1.0)}")
 
                 f.write(f'{i}    {coeffs[0]}    {coeffs[1]}    {coeffs[2]}\n')
 
             f.close()
-
-        test_1 = np.array([0, 0.5, 0.5])
-        test_2 = np.array([0, -0.5, -0.5])
-        A = np.array([[-1, 1, 1],
-                     [1,-1,1],
-                     [1,1,-1]], dtype=np.float64)
-        print(A @ test_1)
-        print(A @ test_2)
+#
+#        test_1 = np.array([0, 0.5, 0.5])
+#        test_2 = np.array([0, -0.5, -0.5])
+#        A = np.array([[-1., 1., 1.],
+#                     [1.,-1.,1.],
+#                     [1.,1.,-1.]], dtype=np.float64)
+#        print(A @ test_1)
+#        print(A @ test_2)